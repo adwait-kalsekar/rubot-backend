30aa06f0978f5809ecd42c464dc2e239a4ed5dea
Adwait Mahesh Kalsekar <ak2521@scarletmail.rutgers.edu>
2024-09-24 00:26:20 -0400

    deployment ready

--- src/core/settings.py
+++ src/core/settings.py
@@ -55,6 +55,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "core.urls"
 
+CORS_URLS_REGEX = r'^/api/v1/.*$'
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
@@ -134,7 +140,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
