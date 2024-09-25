e0b968bdaaeac35e056aebd1c042bdec29c1bf09
Adwait Mahesh Kalsekar <ak2521@scarletmail.rutgers.edu>
2024-09-25 10:34:03 -0400

    wsgi.py path fixed

--- src/core/wsgi.py
+++ src/core/wsgi.py
@@ -1,15 +1,11 @@
-"""
-WSGI config for core project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+# Add the src directory to the Python path
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
